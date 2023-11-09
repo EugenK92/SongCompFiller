57ef3769c33bd6c59c2ee68d59cca867fc22571d
Eugene <e.k.92@gmx.de>
2023-11-09 09:21:34 +0100

    change time sleep into wait

--- insert.py
+++ insert.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.support import expected_conditions
 import time
 import json
 
@@ -29,22 +31,25 @@ except NoSuchElementException:
 with open("output.json", 'r') as file:
     songlist = json.load(file)
 
+errors = [NoSuchElementException, ElementNotInteractableException]
+wait = WebDriverWait(driver, timeout=30, poll_frequency=1, ignored_exceptions=errors)
+submitButtonSelector = '//button[span[text()="Submit "]]'
+goBackButtonSelector = './/button[contains(span/text(), "Go Back")]'
+
 for item in songlist:
 
 	input_name_field = driver.find_element("id", "username")
 	input_name_field.send_keys(Keys.CONTROL + "a")
 	input_name_field.send_keys(Keys.DELETE)
 	input_name_field.send_keys(item["name"])
-	time.sleep(1)
 
 	input_song_field = driver.find_element("id", "submitSong")
 	input_song_field.send_keys(item["link"])
-	time.sleep(8)
-	submit_button = driver.find_element(By.XPATH, '//button[span[text()="Submit "]]')
-	submit_button.click()
-	time.sleep(8)
-	back_button = driver.find_element(By.XPATH, '//button[contains(span/text(), "Go Back")]')
-	back_button.click()
+	
+	wait.until(expected_conditions.element_to_be_clickable((By.XPATH, submitButtonSelector))).click()
+
+	wait.until(expected_conditions.visibility_of_element_located((By.XPATH, goBackButtonSelector))).click()
+	
 	time.sleep(2)
 
 # Close the browser
